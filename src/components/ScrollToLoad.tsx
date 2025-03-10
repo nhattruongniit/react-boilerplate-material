98900325c594e3fd0ec77a9adec5c4e484721352
Tony Nguyen <nhattruong1811@gmail.com>
2025-03-10 20:18:36 +0700

    Update ScrollToLoad.tsx

--- src/components/ScrollToLoad.tsx
+++ src/components/ScrollToLoad.tsx
@@ -1,26 +1,25 @@
-const events = ["mousemove", "scroll"];
+import React, { Fragment, Suspense, useEffect, useState } from 'react';
+
+const events = ["mousemove", "scroll", "keydown", "touchstart"];
 
 function ScrollToLoad({ children }) {
     const [isInteractive, setIsInteractive] = useState(false);
   
     useEffect(() => {
-        const enableInteraction = () => {
-            setIsInteractive(true);
-            cleanup();
-        };
+      const enableInteraction = () => {
+          setIsInteractive(true);
+          cleanup();
+      };
 
-        // add event listeners
-        events.forEach((event) => document.addEventListener(event, enableInteraction));
-      
-        // remove event listeners
-        const cleanup = () => {
-            events.forEach((event) => document.removeEventListener(event, enableInteraction));
-        };
-  
+      events.forEach((event) => document.addEventListener(event, enableInteraction), { once: true });
+    
+      const cleanup = () => {
+          events.forEach((event) => document.removeEventListener(event, enableInteraction));
+      };
+
       return cleanup;
     }, []);
   
-    // return isInteractive ? <>{children}</> : null;
     return <Fragment>{isInteractive ? <Suspense fallback={<></>}>{children}</Suspense> : null}</Fragment>;
   };
   
